f1872849d845a46ed864bf1ba702fb4a29df84e6
nayanarorac <153689095+nayanarorac@users.noreply.github.com>
2025-05-15 10:25:10 -0400

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonaccinumbers(n):
+    if(n == 1 or n== 2):
+        fn = 1
+    else:
+        x = fibonaccinumbers(n-1)
+        y = fibonaccinumbers(n-2)
+        fn = x + y
+    print(fn)
+    return fn
+   
+fibonaccinumbers(15)
